13d2f45b4ecc4d9ec0917fca100bff7c2268fbd6
dqhuy78 <dqhuy78@gmail.com>
2025-02-11 14:14:46 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-neutral-950 flex items-center justify-center text-neutral-100">
+      <div className="w-96 bg-neutral-800 p-4 rounded-lg">
+        // Your component go here
+      </div>
+    </div>
+  )
+}
+
+export default App
